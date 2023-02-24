f499e7d9a3054d99430114175110cacf4aa1de53
Peter Kalverla <peter.kalverla@gmx.com>
2023-02-24 12:32:13 +0100

    Add help and about pages and rename profile page

---
+++ app/routes/_layout.help.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+    return (
+        <p className="prose">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto amet iure temporibus vero dignissimos, dolorem adipisci ipsam suscipit nemo enim a magni ad ipsum. Sint iure itaque assumenda expedita debitis.
+        </p>
+
+    )
+}
