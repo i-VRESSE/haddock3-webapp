e4dd0f1316ee9b01d0ecbc4f86b88ac149d2ad3e
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2023-02-27 15:03:35 +0100

    Use tailwind to style builder

--- app/components/Haddock3/styles.ts
+++ app/components/Haddock3/styles.ts
@@ -1,11 +1,9 @@
-import bootstrapStyles from "bootstrap/dist/css/bootstrap.min.css";
 import wbStyles from "@i-vresse/wb-form/dist/index.css";
 import toastStyles from 'react-toastify/dist/ReactToastify.css'
 import formStyles from './Form.css';
 
 // Moved out of Form.client.tsx as this should be run on server side
 export const haddock3Styles = () => [
-  { rel: "stylesheet", href: bootstrapStyles },
   { rel: "stylesheet", href: wbStyles },
   { rel: "stylesheet", href: formStyles },
   { rel: "stylesheet", href: toastStyles}
