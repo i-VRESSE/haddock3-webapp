c05c7fc155ca37879d16deeb9a3dc263e037fdc5
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2023-04-06 16:52:43 +0200

    Input files can only be viewed once job is completed

--- app/routes/jobs/$id.tsx
+++ app/routes/jobs/$id.tsx
@@ -34,58 +34,61 @@ export default function JobPage() {
         <p>Created on: {job.createdOn}</p>
         <p>Updated on: {job.updatedOn}</p>
       </div>
-      <div>
-        <h2 className="text-xl">Input</h2>
-        <ul className="list-disc list-inside">
-          <li>
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href={`/jobs/${job.id}/files/${WORKFLOW_CONFIG_FILENAME}`}
-            >
-              {WORKFLOW_CONFIG_FILENAME}
-            </a>
-          </li>
-          {/* TODO list files mentioned in workflow config */}
-        </ul>
-      </div>
       {CompletedJobs.has(job.state) && (
-        <div>
-          <h2 className="text-xl">Output</h2>
-          <ListReportFiles
-            files={outputFiles!}
-            prefix={`/jobs/${job.id}/files/`}
-          />
-          <ul className="list-disc list-inside">
-            <li>
-              <a
-                target="_blank"
-                rel="noreferrer"
-                href={`/jobs/${job.id}/stdout`}
-              >
-                Stdout
-              </a>
-            </li>
-            <li>
-              <a
-                target="_blank"
-                rel="noreferrer"
-                href={`/jobs/${job.id}/stderr`}
-              >
-                Stderr
-              </a>
-            </li>
-            <li>
-              <a
-                target="_blank"
-                rel="noreferrer"
-                href={`/jobs/${job.id}/files/output/log`}
-              >
-                Haddock3 log
-              </a>
-            </li>
-          </ul>
-        </div>
+        <>
+          {/* TODO allow to read input files when job is not completed */}
+          <div>
+            <h2 className="text-xl">Input</h2>
+            <ul className="list-disc list-inside">
+              <li>
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href={`/jobs/${job.id}/files/${WORKFLOW_CONFIG_FILENAME}`}
+                >
+                  {WORKFLOW_CONFIG_FILENAME}
+                </a>
+              </li>
+              {/* TODO list files mentioned in workflow config */}
+            </ul>
+          </div>
+          <div>
+            <h2 className="text-xl">Output</h2>
+            <ListReportFiles
+              files={outputFiles!}
+              prefix={`/jobs/${job.id}/files/`}
+            />
+            <ul className="list-disc list-inside">
+              <li>
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href={`/jobs/${job.id}/stdout`}
+                >
+                  Stdout
+                </a>
+              </li>
+              <li>
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href={`/jobs/${job.id}/stderr`}
+                >
+                  Stderr
+                </a>
+              </li>
+              <li>
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href={`/jobs/${job.id}/files/output/log`}
+                >
+                  Haddock3 log
+                </a>
+              </li>
+            </ul>
+          </div>
+        </>
       )}
     </main>
   );
