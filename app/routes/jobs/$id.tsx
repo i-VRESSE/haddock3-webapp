d11d4759308c0bfc6e66f0c59fe4c1f80c845501
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2023-04-06 16:16:47 +0200

    Format

--- app/routes/jobs/$id.tsx
+++ app/routes/jobs/$id.tsx
@@ -16,9 +16,9 @@ export const loader = async ({ params, request }: LoaderArgs) => {
   // TODO check if job belongs to user
   let outputFiles: DirectoryItem | undefined = undefined;
   if (CompletedJobs.has(job.state)) {
-    outputFiles = await listOutputFiles(jobId, accessToken!);    
+    outputFiles = await listOutputFiles(jobId, accessToken!);
   }
-  return json({ job, outputFiles});
+  return json({ job, outputFiles });
 };
 
 export default function JobPage() {
@@ -26,46 +26,64 @@ export default function JobPage() {
   return (
     <main className="flex gap-16">
       <div>
-      <p>Name: {job.name}</p>
-      <p>State: <b>{job.state}</b></p>
-      <p>Created on: {job.createdOn}</p>
-      <p>Updated on: {job.updatedOn}</p>
+        <p>ID: {job.id}</p>
+        <p>Name: {job.name}</p>
+        <p>
+          State: <b>{job.state}</b>
+        </p>
+        <p>Created on: {job.createdOn}</p>
+        <p>Updated on: {job.updatedOn}</p>
       </div>
       <div>
-      <h2 className="text-xl">Input</h2>
-      <ul className="list-disc list-inside">
-      <li>
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href={`/jobs/${job.id}/files/${WORKFLOW_CONFIG_FILENAME}`}
-        >
-          {WORKFLOW_CONFIG_FILENAME}
-        </a>
-      </li>
-      {/* TODO list files mentioned in workflow config */}
-      </ul>
+        <h2 className="text-xl">Input</h2>
+        <ul className="list-disc list-inside">
+          <li>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href={`/jobs/${job.id}/files/${WORKFLOW_CONFIG_FILENAME}`}
+            >
+              {WORKFLOW_CONFIG_FILENAME}
+            </a>
+          </li>
+          {/* TODO list files mentioned in workflow config */}
+        </ul>
       </div>
       {CompletedJobs.has(job.state) && (
         <div>
           <h2 className="text-xl">Output</h2>
-          <ListReportFiles files={outputFiles!} prefix={`/jobs/${job.id}/files/`}/>
+          <ListReportFiles
+            files={outputFiles!}
+            prefix={`/jobs/${job.id}/files/`}
+          />
           <ul className="list-disc list-inside">
             <li>
-            <a target="_blank" rel="noreferrer" href={`/jobs/${job.id}/stdout`}>
-              Stdout
-            </a>
-          </li>
-          <li>
-            <a target="_blank" rel="noreferrer" href={`/jobs/${job.id}/stderr`}>
-              Stderr
-            </a>
-          </li>
-          <li>
-            <a target="_blank" rel="noreferrer" href={`/jobs/${job.id}/files/output/log`}>
-              Haddock3 log
-            </a>
-          </li>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href={`/jobs/${job.id}/stdout`}
+              >
+                Stdout
+              </a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href={`/jobs/${job.id}/stderr`}
+              >
+                Stderr
+              </a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href={`/jobs/${job.id}/files/output/log`}
+              >
+                Haddock3 log
+              </a>
+            </li>
           </ul>
         </div>
       )}
