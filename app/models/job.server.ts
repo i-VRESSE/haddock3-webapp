7043ed228f9455290c63872b76dd3e0af4b87ffd
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2023-06-27 08:49:24 +0200

    Dedup

--- app/models/job.server.ts
+++ app/models/job.server.ts
@@ -15,7 +15,7 @@ export async function getJobs(accessToken: string, limit = 10, offset = 0) {
   });
 }
 
-function handleResponseError(error: unknown): never {
+function handleApiError(error: unknown): never {
   if (error instanceof ResponseError) {
     throw new Response(null, {
       status: error.response.status,
@@ -25,39 +25,34 @@ function handleResponseError(error: unknown): never {
   throw error;
 }
 
-export async function getJobById(jobid: number, accessToken: string) {
+async function safeApi<R>(
+  accessToken: string,
+  fn: (api: JobApi) => Promise<R>
+): Promise<R> {
   const api = buildJobApi(accessToken);
   try {
-    return await api.retrieveJob({
-      jobid,
-    });
+    return await fn(api);
   } catch (error) {
-    handleResponseError(error);
+    handleApiError(error);
   }
+}
+
+export async function getJobById(jobid: number, accessToken: string) {
+  return await safeApi(accessToken, (api) => api.retrieveJob({ jobid }));
 }
 
 export async function getJobStdout(jobid: number, accessToken: string) {
-  const api = buildJobApi(accessToken);
-  try {
-    const response = await api.retrieveJobStdoutRaw({
-      jobid,
-    });
+  return await safeApi(accessToken, async (api) => {
+    const response = await api.retrieveJobStdoutRaw({ jobid });
     return response.raw;
-  } catch (error) {
-    handleResponseError(error);
-  }
+  });
 }
 
 export async function getJobStderr(jobid: number, accessToken: string) {
-  const api = buildJobApi(accessToken);
-  try {
-    const response = await api.retrieveJobStderrRaw({
-      jobid,
-    });
+  return await safeApi(accessToken, async (api) => {
+    const response = await api.retrieveJobStderrRaw({ jobid });
     return response.raw;
-  } catch (error) {
-    handleResponseError(error);
-  }
+  });
 }
 
 export async function getJobfile(
@@ -65,50 +60,37 @@ export async function getJobfile(
   path: string,
   accessToken: string
 ) {
-  const api = buildJobApi(accessToken);
-  try {
-    const response = await api.retrieveJobFilesRaw({
-      jobid,
-      path,
-    });
+  return await safeApi(accessToken, async (api) => {
+    const response = await api.retrieveJobFilesRaw({ jobid, path });
     return response.raw;
-  } catch (error) {
-    handleResponseError(error);
-  }
+  });
 }
 
 export async function listOutputFiles(jobid: number, accessToken: string) {
-  const api = buildJobApi(accessToken);
-  try {
+  return await safeApi(accessToken, async (api) => {
     const items = await api.retrieveJobDirectoriesFromPath({
       jobid,
       path: JOB_OUTPUT_DIR,
       maxDepth: 3,
     });
     return items;
-  } catch (error) {
-    handleResponseError(error);
-  }
+  });
 }
 
 export async function getArchive(jobid: number, accessToken: string) {
-  const api = buildJobApi(accessToken);
-  try {
+  return await safeApi(accessToken, async (api) => {
     const response = await api.retrieveJobDirectoryAsArchiveRaw({
       jobid,
       archiveFormat: ".zip",
     });
     return response.raw;
-  } catch (error) {
-    handleResponseError(error);
-  }
+  });
 }
 
 export async function getInputArchive(jobid: number, accessToken: string) {
-  const api = buildJobApi(accessToken);
   const exclude = ["stderr.txt", "stdout.txt", "meta", "returncode"];
   const excludeDirs = [JOB_OUTPUT_DIR];
-  try {
+  return await safeApi(accessToken, async (api) => {
     const response = await api.retrieveJobDirectoryAsArchiveRaw({
       jobid,
       exclude,
@@ -116,29 +98,24 @@ export async function getInputArchive(jobid: number, accessToken: string) {
       archiveFormat: ".zip",
     });
     return response.raw;
-  } catch (error) {
-    handleResponseError(error);
-  }
+  });
 }
 
 export async function getOutputArchive(jobid: number, accessToken: string) {
-  return await getSubDirectoryAsArchive(
-    jobid,
-    JOB_OUTPUT_DIR,
-    accessToken
-  )
+  return await getSubDirectoryAsArchive(jobid, JOB_OUTPUT_DIR, accessToken);
 }
 
-export async function getSubDirectoryAsArchive(jobid: number, path: string, accessToken: string) {
-  const api = buildJobApi(accessToken);
-  try {
+export async function getSubDirectoryAsArchive(
+  jobid: number,
+  path: string,
+  accessToken: string
+) {
+  return await safeApi(accessToken, async (api) => {
     const response = await api.retrieveJobSubdirectoryAsArchiveRaw({
       jobid,
       path,
       archiveFormat: ".zip",
     });
     return response.raw;
-  } catch (error) {
-    handleResponseError(error);
-  }
+  });
 }
