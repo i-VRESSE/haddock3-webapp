4b7f6f2df57b2b117d01d8fe5d007e6b61c6bd20
sverhoeven <s.verhoeven@esciencecenter.nl>
2023-07-28 14:18:17 +0200

    Use verified email from GH account

--- app/auth.server.ts
+++ app/auth.server.ts
@@ -1,5 +1,8 @@
 import { Authenticator } from "remix-auth";
-import { GitHubStrategy } from "remix-auth-github";
+import {
+  type GitHubEmails,
+  GitHubStrategy,
+} from "remix-auth-github";
 import { FormStrategy } from "remix-auth-form";
 import { json } from "@remix-run/node";
 
@@ -38,21 +41,52 @@ authenticator.use(
   "user-pass"
 );
 
+/**
+ * The super class GitHubStrategy returns emails that are not verified.
+ * This subclass filters out unverified emails.
+ */
+class GitHubStrategyWithVerifiedEmail extends GitHubStrategy<string> {
+  // From https://github.com/sergiodxa/remix-auth-github/blob/75cedd281b58523c5d3db5f7bbe92218cb733c46/src/index.ts#L197
+  protected async userEmails(accessToken: string): Promise<GitHubEmails> {
+    // url & agent are private to super class so we have to copy them here
+    const userEmailsURL = "https://api.github.com/user/emails";
+    const userAgent = "Haddock3WebApp";
+    let response = await fetch(userEmailsURL, {
+      headers: {
+        Accept: "application/vnd.github.v3+json",
+        Authorization: `token ${accessToken}`,
+        "User-Agent": userAgent,
+      },
+    });
+
+    let data: {
+      email: string;
+      verified: boolean;
+      primary: boolean;
+      visibility: string;
+    }[] = await response.json();
+    let emails: GitHubEmails = data
+      .filter((e) => e.verified)
+      .map(({ email }) => ({ value: email }));
+    return emails;
+  }
+}
+
 if (
   process.env.HADDOCK3WEBAPP_GITHUB_CLIENT_ID &&
   process.env.HADDOCK3WEBAPP_GITHUB_CLIENT_SECRET
 ) {
-  let gitHubStrategy = new GitHubStrategy(
+  let gitHubStrategy = new GitHubStrategyWithVerifiedEmail(
     {
       clientID: process.env.HADDOCK3WEBAPP_GITHUB_CLIENT_ID,
       clientSecret: process.env.HADDOCK3WEBAPP_GITHUB_CLIENT_SECRET!,
       callbackURL:
         process.env.HADDOCK3WEBAPP_GITHUB_CALLBACK_URL ||
         "http://localhost:3000/auth/github/callback",
+      userAgent: "Haddock3WebApp",
     },
     async ({ profile }) => {
       // TODO store photo or avatar so it can be displayed in NavBar
-      // TODO fetch verified email not just first email
       const primaryEmail = profile.emails[0].value;
       const userId = await oauthregister(primaryEmail);
       return userId;
