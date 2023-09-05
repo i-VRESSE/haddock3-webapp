ecddd94667a92bc161d93d6c9dcdb83983582505
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2023-09-05 15:30:25 +0200

    Generate client from https://github.com/i-VRESSE/bartender/pull/78/commits/3189ba7dea8ea2514e76604ed1aaf67d9016fe58

--- app/bartender-client/models/index.ts
+++ app/bartender-client/models/index.ts
@@ -1,9 +1,11 @@
 /* tslint:disable */
 /* eslint-disable */
-export * from "./ApplicatonConfiguration";
-export * from "./DirectoryItem";
-export * from "./HTTPValidationError";
-export * from "./JobModelDTO";
-export * from "./LocationInner";
-export * from "./User";
-export * from "./ValidationError";
+export * from './ApplicatonConfiguration';
+export * from './DirectoryItem';
+export * from './HTTPValidationError';
+export * from './InteractiveAppResult';
+export * from './InteractiveApplicationConfiguration';
+export * from './JobModelDTO';
+export * from './LocationInner';
+export * from './User';
+export * from './ValidationError';
