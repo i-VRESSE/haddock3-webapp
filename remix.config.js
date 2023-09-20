16ea29e00744dbaba6ce8ccb5a6eda4552abf3ba
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2023-09-20 10:47:07 +0200

    Incomplete upgrade to remix v2

--- remix.config.js
+++ remix.config.js
@@ -6,13 +6,4 @@ module.exports = {
   // serverBuildPath: "build/index.js",
   // publicPath: "/build/",
   tailwind: true,
-  serverModuleFormat: "cjs",
-  future: {
-    v2_dev: true,
-    v2_errorBoundary: true,
-    v2_headers: true,
-    v2_meta: true,
-    v2_normalizeFormMethod: true,
-    v2_routeConvention: true,
-  },
 };
