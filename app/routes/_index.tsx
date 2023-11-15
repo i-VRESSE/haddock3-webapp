e09b2533753fc04856df64c1f98faa882db7e015
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2023-11-15 13:05:45 +0100

    Use local images

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,24 +1,24 @@
 import Card from "~/components/Card";
+import builder_image from "~/components/cards/builder.png";
+import upload_image from "~/components/cards/upload.svg";
+import manage_image from "~/components/cards/manage.jpg";
 
-// TODO use local images, svgs or unicode icons
 const cards = [
   {
     target: "/builder",
-    image: "https://static.thenounproject.com/png/1781890-200.png",
+    image: builder_image,
     title: "Build",
     description: "Use the workflow builder to create and submit a job.",
   },
   {
     target: "/upload",
-    image:
-      "https://www.filemail.com/images/marketing/anonymously-upload-files.svg",
+    image: upload_image,
     title: "Upload",
     description: "Upload a workflow and submit as job.",
   },
   {
     target: "/jobs",
-    image:
-      "https://www.strategie-bourse.com/nl/images/categories/technische-analyse.jpg",
+    image: manage_image,
     title: "Manage",
     description: "Explore and analyse the results of completed jobs.",
   },
