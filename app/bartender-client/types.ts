25b21391e998895229d5c243a63c21e9334c1714
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2024-01-16 12:07:51 +0100

    fetch new catalogs + automate format

---
+++ app/bartender-client/types.ts
@@ -0,0 +1,7 @@
+/**
+ * Unnest types from the OpenAPI schema
+ */
+import type { components } from "./bartenderschema";
+
+export type JobModelDTO = components["schemas"]["JobModelDTO"];
+export type DirectoryItem = components["schemas"]["DirectoryItem"];
