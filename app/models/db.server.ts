5c52a8dd2165a635a3d3f788e585702823d8dfc4
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2024-01-23 10:50:52 +0100

    Update prismaclient code
    Copied from https://github.com/remix-run/blues-stack/blob/19f60838eadbfaef9bfc08473ca5fb6f03c7c726/app/db.server.ts

--- app/models/db.server.ts
+++ app/models/db.server.ts
@@ -1,23 +1,51 @@
 import { PrismaClient } from "@prisma/client";
+import invariant from "tiny-invariant";
 
-let db: PrismaClient;
+import { singleton } from "../singleton.server";
 
-declare global {
-  var __db__: PrismaClient | undefined;
-}
+// Hard-code a unique key, so we can look up the client when this module gets re-imported
+const db = singleton("db", getPrismaClient);
 
-// This is needed because in development we don't want to restart
-// the server with every change, but we want to make sure we don't
-// create a new connection to the DB with every change either.
-// In production, we'll have a single connection to the DB.
-if (process.env.NODE_ENV === "production") {
-  db = new PrismaClient();
-} else {
-  if (!global.__db__) {
-    global.__db__ = new PrismaClient();
+function getPrismaClient() {
+  const { DATABASE_URL } = process.env;
+  invariant(typeof DATABASE_URL === "string", "DATABASE_URL env var not set");
+
+  const databaseUrl = new URL(DATABASE_URL);
+
+  const isLocalHost = databaseUrl.hostname === "localhost";
+
+  const PRIMARY_REGION = isLocalHost ? null : process.env.PRIMARY_REGION;
+  const FLY_REGION = isLocalHost ? null : process.env.FLY_REGION;
+
+  const isReadReplicaRegion = !PRIMARY_REGION || PRIMARY_REGION === FLY_REGION;
+
+  if (!isLocalHost) {
+    if (databaseUrl.host.endsWith(".internal")) {
+      databaseUrl.host = `${FLY_REGION}.${databaseUrl.host}`;
+    }
+
+    if (!isReadReplicaRegion) {
+      // 5433 is the read-replica port
+      databaseUrl.port = "5433";
+    }
   }
-  db = global.__db__;
-  db.$connect();
+
+  console.log(`🔌 setting up prisma client to ${databaseUrl.host}`);
+  // NOTE: during development if you change anything in this function, remember
+  // that this only runs once per server restart and won't automatically be
+  // re-run per request like everything else is. So if you need to change
+  // something in this file, you'll need to manually restart the server.
+  const client = new PrismaClient({
+    datasources: {
+      db: {
+        url: databaseUrl.toString(),
+      },
+    },
+  });
+  // connect eagerly
+  client.$connect();
+
+  return client;
 }
 
 export { db };
