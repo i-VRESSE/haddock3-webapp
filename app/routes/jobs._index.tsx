050f1303073a7fd2788472250d9b5e17ad3e3d90
sverhoeven <s.verhoeven@esciencecenter.nl>
2024-03-08 14:41:10 +0100

    Make jobs table with delete, rename actions, paged, sortable

--- app/routes/jobs._index.tsx
+++ app/routes/jobs._index.tsx
@@ -1,7 +1,27 @@
 import { json, type LoaderFunctionArgs } from "@remix-run/node";
 import { Link, useFetcher, useLoaderData } from "@remix-run/react";
+import {
+  Column,
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  SortingState,
+  useReactTable,
+} from "@tanstack/react-table";
+import {
+  ArrowDownIcon,
+  ArrowUpIcon,
+  ArrowDownUpIcon,
+  XIcon,
+  PencilIcon,
+} from "lucide-react";
+
+import { PropsWithChildren, useState } from "react";
 
 import { getBartenderToken } from "~/bartender-client/token.server";
+import { JobModelDTO } from "~/bartender-client/types";
 import { Button } from "~/components/ui/button";
 import {
   Table,
@@ -11,6 +31,7 @@ import {
   TableHeader,
   TableRow,
 } from "~/components/ui/table";
+import { cn } from "~/lib/utils";
 import { getJobs } from "~/models/job.server";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
@@ -19,8 +40,180 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
   return json({ jobs });
 };
 
-export default function JobPage() {
+const columnHelper = createColumnHelper<JobModelDTO>();
+
+function JobLink({ jobid, children }: PropsWithChildren<{ jobid: number }>) {
+  return (
+    <Link className="hover:underline" to={`/jobs/${jobid}`}>
+      {children}
+    </Link>
+  );
+}
+
+function ColumnHeader({
+  column,
+  title,
+  className,
+}: {
+  column: Column<JobModelDTO>;
+  title: string;
+  className?: string;
+}) {
+  if (!column.getCanSort()) {
+    return <div className={cn("text-center", className)}>{title}</div>;
+  }
+
+  return (
+    <div className={cn("flex items-center space-x-2", className)}>
+      <Button
+        variant="ghost"
+        size="sm"
+        className="-ml-3 h-8 data-[state=open]:bg-accent"
+        onClick={() => {
+          column.toggleSorting(column.getIsSorted() === "asc");
+        }}
+      >
+        <span>{title}</span>
+        {column.getIsSorted() === "desc" ? (
+          <ArrowDownIcon className="ml-2 h-4 w-4" />
+        ) : column.getIsSorted() === "asc" ? (
+          <ArrowUpIcon className="ml-2 h-4 w-4" />
+        ) : (
+          <ArrowDownUpIcon className="ml-2 h-4 w-4" />
+        )}
+      </Button>
+    </div>
+  );
+}
+
+function RenameAction({
+  onRename,
+  disabled,
+}: {
+  onRename: () => void;
+  disabled: boolean;
+}) {
+  return (
+    <Button
+      disabled={disabled}
+      variant="ghost"
+      onClick={onRename}
+      title="Rename job"
+      size="icon"
+    >
+      <PencilIcon size={16} />
+    </Button>
+  );
+}
+
+function DeleteAction({
+  onDelete,
+  disabled,
+}: {
+  onDelete: () => void;
+  disabled: boolean;
+}) {
+  return (
+    <Button
+      variant="ghost"
+      disabled={disabled}
+      onClick={onDelete}
+      title="Delete/cancel job"
+      size="icon"
+    >
+      <XIcon size={16} />
+    </Button>
+  );
+}
+
+export default function JobsPage() {
   const { jobs } = useLoaderData<typeof loader>();
+  const columns = [
+    columnHelper.accessor("id", {
+      cell: ({ row }) => (
+        <JobLink jobid={row.original.id}>{row.original.id}</JobLink>
+      ),
+      header: ({ column }) => <ColumnHeader column={column} title="ID" />,
+    }),
+    columnHelper.accessor("state", {
+      header: ({ column }) => <ColumnHeader column={column} title="Status" />,
+      cell: ({ row }) => (
+        <JobLink jobid={row.original.id}>{row.original.state}</JobLink>
+      ),
+    }),
+    columnHelper.accessor("name", {
+      header: ({ column }) => <ColumnHeader column={column} title="Name" />,
+      cell: ({ row }) => (
+        <JobLink jobid={row.original.id}>{row.original.name}</JobLink>
+      ),
+    }),
+    columnHelper.accessor("created_on", {
+      header: ({ column }) => (
+        <ColumnHeader column={column} title="Created on" />
+      ),
+      cell: ({ row }) => (
+        <JobLink jobid={row.original.id}>
+          {new Date(row.original.created_on).toUTCString()}
+        </JobLink>
+      ),
+    }),
+    columnHelper.accessor("updated_on", {
+      header: ({ column }) => (
+        <ColumnHeader column={column} title="Updated on" />
+      ),
+      cell: ({ row }) => (
+        <JobLink jobid={row.original.id}>
+          {new Date(row.original.updated_on).toUTCString()}
+        </JobLink>
+      ),
+    }),
+    columnHelper.display({
+      id: "actions",
+      header: ({ column }) => <ColumnHeader column={column} title="Actions" />,
+      cell: ({ row }) => {
+        // job in new or staging_out state cannot be deleted, you have to wait for job to switch to another state
+        const disabled =
+          state === "submitting" ||
+          row.original.state === "new" ||
+          row.original.state === "staging_out";
+        return (
+          <div className="flex items-center justify-center">
+            <RenameAction
+              disabled={state === "submitting"}
+              onRename={() => renameJob(row.original.id, row.original.name)}
+            />
+            <DeleteAction
+              onDelete={() => deleteJob(row.original.id)}
+              disabled={disabled}
+            />
+          </div>
+        );
+      },
+    }),
+  ];
+  const [sorting, setSorting] = useState<SortingState>([
+    {
+      id: "id",
+      desc: true,
+    },
+  ]);
+  const table = useReactTable({
+    data: jobs,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    state: {
+      sorting,
+    },
+    initialState: {
+      pagination: {
+        pageSize: 6,
+      },
+    },
+  });
+
   const { submit, state } = useFetcher();
 
   function deleteJob(id: number): void {
@@ -30,57 +223,75 @@ export default function JobPage() {
     submit({}, { method: "delete", action: `/jobs/${id}` });
   }
 
-  // TODO add pagination, usefull for large number of jobs
+  function renameJob(id: number, name: string): void {
+    const newName = window.prompt("Enter new name for the job", name);
+    if (newName === null) {
+      return;
+    }
+    const data = new FormData();
+    data.set("name", newName);
+    submit(data, { method: "post", action: `/jobs/${id}/name` });
+  }
+
   return (
     <main>
       <Table>
         <TableHeader>
-          <TableRow>
-            <TableHead>ID</TableHead>
-            <TableHead>Status</TableHead>
-            <TableHead>Name</TableHead>
-            <TableHead>Created on</TableHead>
-            <TableHead>Updated on</TableHead>
-            <TableHead>Actions</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {jobs.map((job) => (
-            <TableRow key={job.id}>
-              <TableCell>
-                <Link to={`/jobs/${job.id}`}>{job.id}</Link>
-              </TableCell>
-              <TableCell>
-                <Link to={`/jobs/${job.id}`}>{job.state}</Link>
-              </TableCell>
-              <TableCell>
-                <Link to={`/jobs/${job.id}`}>{job.name}</Link>
-              </TableCell>
-              <TableCell>
-                <Link to={`/jobs/${job.id}`}>
-                  {new Date(job.created_on).toUTCString()}
-                </Link>
-              </TableCell>
-              <TableCell>
-                <Link to={`/jobs/${job.id}`}>
-                  {new Date(job.updated_on).toUTCString()}
-                </Link>
-              </TableCell>
-              <TableCell>
-                <Button
-                  disabled={state === "submitting"}
-                  onClick={() => deleteJob(job.id)}
-                  variant="secondary"
-                  title="Delete/cancel job"
-                  size="sm"
-                >
-                  X
-                </Button>
-              </TableCell>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <TableHead key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                  </TableHead>
+                );
+              })}
             </TableRow>
           ))}
+        </TableHeader>
+        <TableBody>
+          {table.getRowModel().rows?.length ? (
+            table.getRowModel().rows.map((row) => (
+              <TableRow key={row.id}>
+                {row.getAllCells().map((cell) => (
+                  <TableCell key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={columns.length} className="h-24 text-center">
+                No jobs.
+              </TableCell>
+            </TableRow>
+          )}
         </TableBody>
       </Table>
+      <div className="flex items-center justify-end space-x-2 py-4">
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => table.previousPage()}
+          disabled={!table.getCanPreviousPage()}
+        >
+          Previous
+        </Button>
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => table.nextPage()}
+          disabled={!table.getCanNextPage()}
+        >
+          Next
+        </Button>
+      </div>
     </main>
   );
 }
