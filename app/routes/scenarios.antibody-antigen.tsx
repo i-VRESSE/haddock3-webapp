3d7ed23e797117d020aebee9b1280086679f5a0e
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2024-03-19 13:48:22 +0100

    Working first scenario
    * pdb form field has preview in 3D button
    * submit via upload action
    * refine with staging zip in indexeddb and loading it in builder
    * download zip button

--- app/routes/scenarios.antibody-antigen.tsx
+++ app/routes/scenarios.antibody-antigen.tsx
@@ -1,13 +1,28 @@
-import { Form } from "@remix-run/react";
+import {
+  NavigateFunction,
+  SubmitFunction,
+  useActionData,
+  useNavigate,
+  useSubmit,
+} from "@remix-run/react";
+import JSZip from "jszip";
 import { PropsWithChildren, useState } from "react";
-import { FlatErrors } from "valibot";
+import { FlatErrors, Output, instance, mimeType, object, parse } from "valibot";
+import { WORKFLOW_CONFIG_FILENAME } from "~/bartender-client/constants";
 
 import { ErrorMessages } from "~/components/ErrorMessages";
 import { Button } from "~/components/ui/button";
 import { Input } from "~/components/ui/input";
 import { Label } from "~/components/ui/label";
-import { Molecule3D } from "~/scenario-antibody-antigen/Molecule3D.client";
 import { Viewer } from "~/scenario-antibody-antigen/Viewer.client";
+import { action as uploadaction } from "./upload";
+import {
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+} from "~/components/ui/dialog";
+
+export const action = uploadaction;
 
 function FormItem({
   name,
@@ -28,16 +43,270 @@ function FormItem({
   );
 }
 
-// TODO from formdata construct archive with workflow config
-// which looks like docking-antibody-antigen-CDR-accessible-full.cfg
+function FormDescription({ children }: PropsWithChildren): JSX.Element {
+  return <p className="text-[0.8rem] text-muted-foreground">{children}</p>;
+}
+
+function PDBFileInput({
+  name,
+  required,
+}: {
+  name: string;
+  required?: boolean;
+}) {
+  const [file, setFile] = useState<File | undefined>(undefined);
+  const [open, setOpen] = useState(false);
+
+  function onChange(event: React.ChangeEvent<HTMLInputElement>) {
+    event.preventDefault();
+    setFile(event.target.files?.[0]);
+  }
+
+  return (
+    <div className="flex">
+      <Input
+        type="file"
+        id={name}
+        name={name}
+        required={required}
+        accept=".pdb"
+        onChange={onChange}
+      />
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger asChild>
+          <Button
+            disabled={!file}
+            title="Preview in 3D"
+            variant="ghost"
+            size="icon"
+          >
+            👁
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="max-w-2/3 h-2/3 w-2/3">
+          {file !== undefined && open && <Viewer file={file} />}
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+}
+
+function generateWorkflow(data: Schema) {
+  // TODO create workflow.cfg with form data as values for filename fields
+
+  // Workflow based on
+  // scenario2a-NMR-epitope-pass-short.cfg
+  // in https://surfdrive.surf.nl/files/index.php/s/HvXxgxCTY1DiPsV
+  // from
+  // https://www.bonvinlab.org/education/HADDOCK3/HADDOCK3-antibody-antigen/#setuprequirements
+  // but made valid for easy expertise level
+  return `
+# ====================================================================
+# Antibody-antigen docking example with restraints from the antibody
+# paratope to the NMR-identified epitope on the antigen (as passive)
+# ====================================================================
+
+# directory name of the run
+run_dir = "scenario2a-NMR-epitope-pass-short"
+
+# Compute mode
+mode = "local"
+# 10 cores
+ncores = 10
+
+# Self contained rundir
+#self_contained = false
+
+# Post-processing to generate statistics and plots
+postprocess = true
+
+# Cleaning
+clean = true
+
+# molecules to be docked
+molecules =  [
+    "${data.antibody.name}",
+    "${data.antigen.name}"
+    ]
+
+# ====================================================================
+# Parameters for each stage are defined below, prefer full paths
+# ====================================================================
+[topoaa]
+
+[rigidbody]
+# number of models to generate
+sampling = 200
+# paratope to surface ambig restraints
+ambig_fname = "${data.ambig_fname.name}"
+# Restraints to keep the antibody chains together
+unambig_fname = "${data.unambig_fname.name}"
+# Turn off ramdom removal of restraints
+randremoval = false
+
+[clustfcc]
+min_population = 10
+
+[seletopclusts]
+## select all the clusters
+top_cluster = 500
+## select the best 10 models of each cluster
+top_models = 10
+
+[caprieval]
+# this is only for this tutorial to check the performance at the rigidbody stage
+reference_fname = "${data.reference_fname.name}"
+
+[flexref]
+# Acceptable percentage of model failures
+tolerance = 5
+# paratope to surface ambig restraints
+ambig_fname = "${data.ambig_fname.name}"
+# Restraints to keep the antibody chains together
+unambig_fname = "${data.unambig_fname.name}"
+# Turn off ramdom removal of restraints
+randremoval = false
+
+[emref]
+# paratope to surface ambig restraints
+ambig_fname = "${data.ambig_fname.name}"
+# Restraints to keep the antibody chains together
+unambig_fname = "${data.unambig_fname.name}"
+# Turn off ramdom removal of restraints
+randremoval = false
+
+[clustfcc]
+
+[seletopclusts]
+top_cluster = 500
+
+[caprieval]
+reference_fname = "${data.reference_fname.name}"
+
+# ====================================================================
+
+`;
+}
+
+const pdbMimeType = mimeType<File>(
+  [
+    "chemical/x-pdb",
+    "chemical/x-pdbx",
+    "application/vnd.palm",
+    "application/x-aportisdoc",
+  ],
+  "Please select a PDB file"
+);
+
+const Schema = object({
+  antibody: instance(File, "Antibody molecules as PDB file", [pdbMimeType]),
+  antigen: instance(File, "Antibody molecules as PDB file", [pdbMimeType]),
+  // restraints get type==='' so cannot check for file type
+  ambig_fname: instance(File, "Ambiguous restraints as TBL file"),
+  unambig_fname: instance(File, "Unambiguous restraints as TBL file"),
+  reference_fname: instance(File, "Reference structure as PDB file", [
+    pdbMimeType,
+  ]),
+});
+type Schema = Output<typeof Schema>;
+
+function parseFormData(formData: FormData) {
+  const obj = Object.fromEntries(formData.entries());
+  return parse(Schema, obj);
+}
+
+async function createZip(workflow: string, data: Schema) {
+  const zip = new JSZip();
+  zip.file(WORKFLOW_CONFIG_FILENAME, workflow);
+  zip.file(data.antibody.name, data.antibody);
+  zip.file(data.antigen.name, data.antigen);
+  zip.file(data.ambig_fname.name, data.ambig_fname);
+  zip.file(data.unambig_fname.name, data.unambig_fname);
+  zip.file(data.reference_fname.name, data.reference_fname);
+  return zip.generateAsync({ type: "blob" });
+}
+
+function doUpload(zipPromise: Promise<Blob>, submit: SubmitFunction) {
+  zipPromise.then((zip) => {
+    // TODO upload archive to server
+    const formData = new FormData();
+    formData.set("upload", zip);
+    submit(formData, {
+      method: "post",
+      encType: "multipart/form-data",
+    });
+  });
+}
+
+function onRefine(zipPromise: Promise<Blob>, navigate: NavigateFunction) {
+  // add zip to browsers indexeddb haddock3 db, zips object store, key workflow.zip
+  if (typeof indexedDB === "undefined") {
+    console.error("IndexedDB not supported, unable to save workflow.zip file.");
+    return;
+  }
+
+  const open = indexedDB.open("haddock3", 1);
+  open.onerror = function () {
+    console.error("Error opening indexeddb", open.error);
+  };
+  open.onupgradeneeded = function () {
+    const db = open.result;
+    db.createObjectStore("zips");
+  };
+  open.onblocked = function () {
+    console.error("Error opening indexeddb, blocked");
+  };
+  open.onsuccess = function () {
+    zipPromise.then((zip) => {
+      const db = open.result;
+      const tx = db.transaction("zips", "readwrite");
+      const zips = tx.objectStore("zips");
+      const putRequest = zips.put(zip, "workflow.zip");
+      putRequest.onerror = function () {
+        console.error("Error putting zip in indexeddb", putRequest.error);
+      };
+      putRequest.onsuccess = function () {
+        navigate("/builder");
+      };
+    });
+  };
+}
+
+function onDownload(zipPromise: Promise<Blob>) {
+  zipPromise.then((zip) => {
+    const url = URL.createObjectURL(zip);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "workflow.zip";
+    a.click();
+    URL.revokeObjectURL(url);
+  });
+}
 
 export default function AntibodyAntigenScenario() {
-    const [antibodyFile, setAntibodyFile] = useState<File | undefined>(undefined)
-  
-    function onAntibodyChange(event: React.ChangeEvent<HTMLInputElement>) {
-        event.preventDefault()
-        setAntibodyFile(event.target.files?.[0])
+  const actionData = useActionData<typeof uploadaction>();
+  const submit = useSubmit();
+  const navigate = useNavigate();
+
+  function onSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const form = event.currentTarget;
+    const formData = new FormData(form);
+    const data = parseFormData(formData);
+    const workflow = generateWorkflow(data);
+    const zipPromise = createZip(workflow, data);
+    // TODO validate zip against catalog, now done on server for kind=upload
+    // or on /builder page in devtools console for kind=refine
+    const nativeEvent = event.nativeEvent as SubmitEvent;
+    const kind = nativeEvent.submitter?.getAttribute("value");
+    if (kind === "refine") {
+      onRefine(zipPromise, navigate);
+    } else if (kind === "download") {
+      onDownload(zipPromise);
+    } else {
+      doUpload(zipPromise, submit);
     }
+  }
 
   return (
     <div>
@@ -53,82 +322,76 @@ export default function AntibodyAntigenScenario() {
           HADDOCK3 Antibody Antigen tutorial
         </a>
       </p>
-    <Form method="post">
-<div className="grid gap-6 grid-cols-2">
-    <div>
-        <FormItem
-            name="antibody"
-            label="Antibody"
-        >
-            <Input type="file" 
-                id="antibody"
-                name="antibody"
-                required
-                accept=".pdb"
-                onChange={onAntibodyChange}
+      <form onSubmit={onSubmit}>
+        <div className="grid grid-cols-2 gap-6">
+          <FormItem name="antibody" label="Antibody">
+            <PDBFileInput name="antibody" required />
+            <FormDescription>
+              In tutorial named pdbs/4G6K_clean.pdb
+            </FormDescription>
+          </FormItem>
+          <FormItem name="antigen" label="Antigen">
+            <PDBFileInput name="antigen" required />
+            <FormDescription>
+              In tutorial named pdbs/4I1B_clean.pdb
+            </FormDescription>
+          </FormItem>
+          <FormItem name="ambig_fname" label="Ambiguous restraints">
+            <Input
+              type="file"
+              id="ambig_fname"
+              name="ambig_fname"
+              required
+              accept=".tbl"
             />
-        </FormItem>
-        <FormItem
-            name="antigen"
-            label="Antigen"
-        >
-            <Input type="file" 
-                id="antigen"
-                name="antigen"
-                required
-                accept=".pdb"
+            <FormDescription>
+              In tutorial named restraints/ambig-paratope-NMR-epitope-pass.tbl
+            </FormDescription>
+          </FormItem>
+          <FormItem name="unambig_fname" label="Unambiguous restraints">
+            <Input
+              type="file"
+              id="unambig_fname"
+              name="unambig_fname"
+              required
+              accept=".tbl"
             />
-        </FormItem>
-        {/* <FormItem
-            name="ambig_fname"
-            label="Ambiguous restraints"
-        >
-            <Input type="file" 
-                id="ambig_fname"
-                name="ambig_fname"
-                required
-                accept=".tbl"
-            />
-        </FormItem>
-        <FormItem
-            name="unambig_fname"
-            label="Unambiguous restraints"
-        >
-            <Input type="file" 
-                id="unambig_fname"
-                name="unambig_fname"
-                required
-                accept=".tbl"
-            />
-        </FormItem> */}
-        <FormItem
-            name="reference_fname"
-            label="Reference structure"
-        >
-            <Input type="file" 
-                id="reference_fname"
-                name="reference_fname"
-                required
-                accept=".pdb"
-            />
-        </FormItem>
-
-    </div>
-</div>
-        
-         <div className="mt-4">
-        <Button type="submit">Submit</Button>
-        <Button variant="secondary">Refine in builder</Button>
-            </div>   
-      </Form>
-      {(Molecule3D !== undefined && antibodyFile) ?
-      <Molecule3D
-        file={antibodyFile}
-      />
-      :
-      <div className="relative w-[800px] h-[400px]">Loading...</div>
-
-    }
+            <FormDescription>
+              In tutorial named restraints/antibody-unambig.tbl
+            </FormDescription>
+          </FormItem>
+          <FormItem name="reference_fname" label="Reference structure">
+            <PDBFileInput name="reference_fname" />
+            <FormDescription>
+              In tutorial named pdbs/4G6M_matched.pdb
+            </FormDescription>
+          </FormItem>
+        </div>
+        <div className="py-2 text-red-500">
+          {actionData?.errors.map((error) => (
+            <p key={error}>{error}</p>
+          ))}
+        </div>
+        <div className="mt-4">
+          <Button type="submit" name="kind" value="upload">
+            Submit
+          </Button>
+          <Button type="reset" variant="secondary">
+            Reset
+          </Button>
+          <Button type="submit" name="kind" value="refine" variant="secondary">
+            Refine in builder
+          </Button>
+          <Button
+            type="submit"
+            variant="secondary"
+            name="kind"
+            value="download"
+          >
+            Download workfow.zip
+          </Button>
+        </div>
+      </form>
     </div>
   );
 }
--- app/scenario-antibody-antigen/Viewer.client.tsx
+++ app/scenario-antibody-antigen/Viewer.client.tsx
@@ -1,46 +1,15 @@
-import { ChangeEvent, useEffect, useRef, useState } from "react";
-import { Stage, type StructureComponent } from "ngl";
+import { useEffect, useRef, useState } from "react";
+import { Stage } from "ngl";
 
-/*
-import "molstar/lib/mol-plugin-ui/skin/light.scss";
-
-import pdb1 from "./4G6K_fv.pdb?url";
-
-      const trajectory =
-        await window.molstar.builders.structure.parseTrajectory(data, "pdb");
-      const model = await window.molstar.builders.structure.createModel(trajectory);
-      const structure = await window.molstar.builders.structure.createStructure(model);
-
-      // add 3D representation of the paratope selection with type=cartoon, colortheme=uniform, value=red, sizetheme=uniform
-      const paratropeResidues = [31, 32, 33, 34, 35, 52, 54, 55, 56, 100, 101, 102, 103, 104, 105, 106, 1031, 1032, 1049, 1050, 1053, 1091, 1092, 1093, 1094, 1096];
-      const rs = MolScriptBuilder.struct.atomProperty.macromolecular.label_seq_id
-      const expr = MolScriptBuilder.struct.generator.atomGroups({
-        'residue-test': MolScriptBuilder.core.logic.or(
-            paratropeResidues.map(r => MolScriptBuilder.core.rel.eq([rs(), r]))
-        ),
-      })
-      const paratopeComponent = await window.molstar.builders.structure.tryCreateComponentFromExpression(
-          structure, expr, 'paratope'
-      )
-      if (paratopeComponent) {
-        await window.molstar.builders.structure.representation.addRepresentation(
-            paratopeComponent, { type: 'cartoon', color: 'uniform', colorParams: { value: 0xFF0000 }, size: 'uniform' }
-            )
-      }
-      await window.molstar.builders.structure.hierarchy.applyPreset(
-        trajectory,
-        "default"
-      );
-
-*/
-
-export function Viewer({ antibodyFile }: { antibodyFile: File | undefined }) {
+export function Viewer({ file }: { file: File | undefined }) {
   const viewportRef = useRef<HTMLDivElement>(null);
   const stage = useRef<Stage | null>(null);
-  const [antibodyResidues, setAntibodyResidues] = useState<string[]>([]);
-  const [paratropeResidues, setParatropeResidues] = useState<string[]>([]);
+  const [isLoaded, setIsLoaded] = useState(false);
 
   useEffect(() => {
+    if (isLoaded) {
+      return;
+    }
     if (!viewportRef.current) {
       return;
     }
@@ -57,86 +26,32 @@ export function Viewer({ antibodyFile }: { antibodyFile: File | undefined }) {
     }
     stage.current.setParameters({ backgroundColor });
 
-    return () => {
-      if (stage.current) {
-        stage.current.dispose();
-      }
-    };
-  }, []);
-
-  useEffect(() => {
-    if (stage.current === null || antibodyFile === undefined) {
+    if (stage.current === null || file === undefined) {
       return;
     }
-
-    stage.current.loadFile(antibodyFile, { ext: "pdb" }).then((o) => {
+    stage.current.loadFile(file, { ext: "pdb" }).then((o) => {
       if (o === undefined) {
         console.error("Could not load file");
         return;
       }
-      o.addRepresentation("cartoon", { sele: "protein", color: "blue" });
+      o.addRepresentation("cartoon", { sele: "protein" });
       o.addRepresentation("ball+stick", { sele: "ligand" });
+      o.addRepresentation("base", { sele: "nucleic" });
 
       o.autoView();
-
-      const sc = o as StructureComponent;
-      setAntibodyResidues(
-        Array.from(
-          new Set(
-            Array.from(sc.structure.residueStore.resno).map((resno) =>
-              resno.toString()
-            ).filter((resno) => resno !== "0")
-          )
-        )
-      );
-      setParatropeResidues([])
+      setIsLoaded(true);
     });
 
-    return () => {
-      if (stage.current && antibodyFile) {
-        const comps = stage.current.getComponentsByName(antibodyFile.name);
-        comps.dispose();
-      }
-    };
-  }, [antibodyFile]);
+    // return () => {
+    //   if (stage.current && file) {
+    //     const comps = stage.current.getComponentsByName(file.name);
+    //     comps.dispose();
+    //   }
+    //   if (stage.current) {
+    //     stage.current.dispose();
+    //   }
+    // };
+  }, [file, isLoaded]);
 
-  function paratropeOnChange(event: ChangeEvent<HTMLSelectElement>): void {
-    event.preventDefault();
-
-    const selection: string[] = Array.from(
-      event.currentTarget.selectedOptions).map(option => option.value);
-    setParatropeResidues(selection)
-  }
-
-  useEffect(() => {
-    if (stage.current === null || antibodyFile === undefined) {
-      return;
-    }
-
-    const comp = stage.current.getComponentsByName(antibodyFile?.name).first as StructureComponent
-    const sele = paratropeResidues.map(r => `[${r}]`).join(" ")
-    console.log({sele})
-    if (sele === "") {
-      return
-    }
-    comp.addRepresentation("ball+stick", { sele, color: "yellow", labelType: "res" });
-  }, [paratropeResidues])
-
-  return (
-    <div>
-      <div ref={viewportRef} className="h-[800px] w-[800px]"></div>
-      <label>
-        Paratrope residues
-        <select multiple className="w-40 bg-inherit text-inherit"
-          onChange={paratropeOnChange}
-        >
-          {antibodyResidues.map((residue) => (
-            <option key={residue} value={residue}>
-              {residue}
-            </option>
-          ))}
-        </select>
-      </label>
-    </div>
-  );
+  return <div ref={viewportRef} className="h-full w-full"></div>;
 }
