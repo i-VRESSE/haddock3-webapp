fddbd20d26145454153c6a3c27dd2a47474bb20b
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2024-05-10 13:37:22 +0200

    Make prefix work in arq deployment

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ installGlobals();
 const isStorybook = process.argv[1]?.includes("storybook");
 const isTestEnv = process.env.VITEST;
 
-const prefix = process.env.HADDOCK3_PREFIX || "/";
+const prefix = process.env.HADDOCK3_WEBAPP_PREFIX || "/";
 const remixConfig = {
   basename: prefix,
 };
