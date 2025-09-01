79c1c1b05021a78bddf5dd3a11a70c7cd444caac
Rodrigo V. Honorato <r.vargashonorato@uu.nl>
2025-09-01 15:01:37 +0200

    add `PortalWarning.tsx`

--- app/components/PortalHeader.tsx
+++ app/components/PortalHeader.tsx
@@ -31,12 +31,30 @@ function Haddock3Navbar() {
   );
 }
 
+function PortalWarning() {
+  return (
+    <div className="m-2 p-3 bg-yellow-400 border-l-4 border-yellow-600 text-black font-bold rounded-md shadow-md">
+      <div className="flex items-center justify-center">
+        <svg className="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20">
+          <path
+            fillRule="evenodd"
+            d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
+            clipRule="evenodd"
+          />
+        </svg>
+        <span>Development Preview - Features may be unstable</span>
+      </div>
+    </div>
+  );
+}
+
 // Modeled after https://wenmr.science.uu.nl/new/prodigy
 export function Portalheader() {
   return (
     <header>
       <PortalNavbar />
       <Haddock3Navbar />
+      <PortalWarning />
       <Banner />
     </header>
   );
